97fc8b5e49e0937af9671aa754309ee46628bcd4
MembrilloAmarillo <saschapaezsk8@gmail.com>
2023-08-05 17:05:47 +0200

    Some change

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,27 +1,16 @@
 internal int 
 __MAX(int a, int b, int c)
 {
-	if (a >= b) {
-		if (a >= c)
-		{
-			return a;
-		}
-		else
-		{
-			return c;
-		}
-	}
+    if (a >= b)
+        if (a >= c)
+        	return a;
+   		else
+        	return c;
 	else 
-	{
 		if (b >= c) 
-		{
-			return b;
-		} 
-		else
-		{ 
-			return c; 
-		}
-	}
+        	return b;
+    	else
+        	return c; 
 }
 
 internal int
@@ -29,14 +18,14 @@ __MIN(int a, int b, int c)
 {
 	if (a < b)
 		if (a < c)
-			return a;
-		else
-			return c;
+        	return a;
+    	else
+        	return c;
 	else
 		if (b < c)
-			return b;
-		else
-			return c;
+        	return b;
+    	else
+        	return c;
 }
 
 #define MAX(a, b, c) __MAX(a, b, c)
@@ -75,22 +64,22 @@ internal void
 LCS( const char* S, const char* R, size_t SL, size_t RL ) 
 {
 	int *row = global_matrix;
-
+    
 	global_matrix_struct.start = row;
-
+    
 	for (size_t n = 0; n < SL; n++)
 	{
 		*row = 0;
 		row += RL;
 	}
-
+    
 	row = global_matrix;
 	for (size_t n = 0; n < RL; n++)
 	{
 		*row = 0;
 		row += n;
 	}
-
+    
 	row = global_matrix + RL;
 	for (size_t n = 1; n < SL; n++)
 	{
@@ -100,17 +89,17 @@ LCS( const char* S, const char* R, size_t SL, size_t RL )
 			int *val_izq = (row + m - 1);
 			int *val_up  = (row + m - RL);
 			int *val_dia = (row - RL + m - 1);
-
+            
 			*val = MAX(*val_izq, 
-			          *val_up, 
-			          *val_dia + LCS_f(S[n], R[m]) 
-			          );
+                       *val_up, 
+                       *val_dia + LCS_f(S[n], R[m]) 
+                       );
 		}
-
+        
 		row += RL;
 	}
-
-	global_matrix_struct.end = row;
+    
+	global_matrix_struct.end = row - 1;
 }
 
 /* Edit Distance */
@@ -118,22 +107,22 @@ internal void
 ED( const char* S, const char* R, size_t SL, size_t RL )
 {
 	int *row = global_matrix;
-
+    
 	global_matrix_struct.start = row;
-
+    
 	for (size_t n = 0; n <= SL; n++)
 	{
 		*row = n;
 		row += RL + 1;
 	}
-
+    
 	row = global_matrix;
-
+    
 	for (size_t n = 0; n <= RL; n++)
 	{
 		row[n] = n;
 	}
-
+    
 	row = global_matrix + RL + 1;
 	for (size_t n = 1; n <= SL; n++)
 	{
@@ -143,16 +132,16 @@ ED( const char* S, const char* R, size_t SL, size_t RL )
 			int *val_izq = (row + m - 1);
 			int *val_up  = (row - RL + m );
 			int *val_dia = (row - RL + m - 1);
-
+            
 			*val = MIN(*val_izq + 1, 
 			           *val_up  + 1, 
 			           *val_dia + ED_f(S[n-1], R[m-1]) 
 			           );
 		}
-
+        
 		row += RL + 1;
 	}
-
+    
 	global_matrix_struct.end = row - 1;
 }
 
@@ -180,14 +169,14 @@ ED_Backtrack( char* common, const char* S, const char* R, size_t SL, size_t RL )
 	int i = SL - 1;
 	int j = RL - 1;
 	int idx = 0;
-
+    
 	while( i >= 0 && j >= 0 )
 	{
 		//int *val     = row + j;
 		int *val_izq = (row + j);
 		int *val_up  = (row + j - RL);
 		int *val_dia = (row - RL + j);
-
+        
 		if (S[j] != R[i])
 		{
 			if (val_izq <= val_up)
